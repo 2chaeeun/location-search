f494b0f43b0c4da07ab217059a068dea0bd8f155
jinny <22chaeeun@gmail.com>
2023-03-06 23:59:22 +0900

    Initial Commit

---
+++ src/main/java/com/chaeeun/locationsearch/LocationSearchApplication.java
@@ -0,0 +1,13 @@
+package com.chaeeun.locationsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chaeeun/locationsearch/LocationSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaeeun.locationsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
